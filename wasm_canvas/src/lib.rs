b3cd3e275975afa5f36fc4c6908100fc27ebf676
Lars Schumann <LarsSchumann@mail.de>
2025-03-20 22:01:13 +0100

    now with animations

--- wasm_canvas/src/lib.rs
+++ wasm_canvas/src/lib.rs
@@ -1,5 +1,11 @@
+use std::{iter::RepeatN, thread::sleep, time};
+
 use wasm_bindgen::prelude::*;
-use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, window};
+use web_sys::{
+    CanvasRenderingContext2d, Document, HtmlCanvasElement,
+    js_sys::{Math::random, eval},
+    window,
+};
 
 #[wasm_bindgen(start)]
 pub fn start() {
@@ -18,6 +24,31 @@ pub fn start() {
         .unwrap();
 
     // Draw a rectangle
-    context.set_fill_style(&"blue".into());
-    context.fill_rect(50.0, 50.0, 100.0, 100.0);
+    context.set_fill_style(&"yellow".into());
+    context.fill_rect(50.0, 50.0, 55.0, 55.0);
 }
+
+#[wasm_bindgen]
+pub fn noomber() {
+    let document: Document = window().unwrap().document().unwrap();
+    let canvas: HtmlCanvasElement = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<HtmlCanvasElement>()
+        .unwrap();
+
+    let context: CanvasRenderingContext2d = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.set_fill_style(&"blue".into());
+    let (x, y) = (500. * random(), 500. * random());
+    context.fill_rect(x, y, 1., 1.);
+
+    web_sys::console::log1(&String::from("wassup"));
+}
+
+//wasm-pack build --target web --out-dir ../static/wasm
